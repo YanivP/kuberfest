d141ba9e9329f37b0b532e2b4b2222f56bb7f2f9
Yaniv Peer <yanivpeer@gmail.com>
2018-10-20 11:46:32 +0300

    More refactoring

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yanivp/kuberfest",
     packages=setuptools.find_namespace_packages(),
+    install_requires=[
+        'pyyaml'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
