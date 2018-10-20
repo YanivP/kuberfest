b82a0c762364767362181477b2bd78c4cd83cfca
Yaniv Peer <yanivpeer@gmail.com>
2018-10-20 21:08:27 +0300

    Removed redundant code in context.py

--- kuberfest/commands/context.py
+++ kuberfest/commands/context.py
@@ -5,9 +5,6 @@ import os
 
 def run(project, value):
     Debug.info('Switching Kubernetes context...')
-    context = project.get_variable('CONTEXT')
-
-    # TODO: Allow switching manually
     KubernetesTool(project).switch_config(value)
 
     return True
